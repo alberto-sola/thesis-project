b311994381e824d57402628996c052737fd84d72
Alberto Sola <albertosola2002@gmail.com>
2025-12-11 12:11:50 +0100

    New class added in main.py as an example

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
+class Person:
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+    def greet(self) -> str:
+        return f"Hi, I'm {self.name} and I'm {self.age} years old."
+
 
 if __name__ == '__main__':
-    print('Hello World')
-
+    person = Person(name='Ada Lovelace', age=36)
+    print(person.greet())
